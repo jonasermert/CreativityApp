3dd1768e22fecac96e09ee523d7dace910071add
Jonas Ermert <ermert.jonas@yahoo.de>
2022-08-13 12:56:06 +0200

    First steps

---
+++ lib/myhomepage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+class MyHomepage extends StatelessWidget {
+  const MyHomepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
